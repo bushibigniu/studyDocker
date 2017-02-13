4500677a436da9d341143f7a5cc543a816caaaa4
Nicce Zhang <nicce.zhang@appcoachs.com>
2017-02-13 19:16:55 +0800

    update

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 if [ ! -f docker-compose.yml ]; then
 	cp docker-compose.yml.dist.v2 docker-compose.yml
-#elif [ -f docker-compose.yml ]; then
-#	rm -rf docker-compose.yml
-#	echo 'rm ok'
+elif [ -f docker-compose.yml ]; then
+	rm -rf docker-compose.yml
+	echo 'rm ok'
 else 
 	cp docker-compose.yml.dist.v1 docker-compose.yml
 fi
